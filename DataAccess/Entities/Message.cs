970d68c299a9c4c114e8378d50e10ca8d3fac64e
Sanan Tahmazli <sanan.tahmazli@khazar.org>
2023-03-14 00:58:05 +0400

    Creating EmailConfiguration and Message entities

---
+++ DataAccess/Entities/Message.cs
@@ -0,0 +1,24 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Message
+    {
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public Message(IEnumerable<string> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+            To.AddRange(to.Select(t => new MailboxAddress("company.bookify@gmail.com", t)));
+            Subject = subject;
+            Content = content;
+        }
+    }
+}
